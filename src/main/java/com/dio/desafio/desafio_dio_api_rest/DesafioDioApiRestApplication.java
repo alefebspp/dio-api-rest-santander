8275a4dbb768a61f1fc442f63eeef8f3fe88fd01
Alefe Bispo <alefe.bispo@onesight.com.br>
2024-07-19 19:48:39 -0300

    Bug fix CORS

--- src/main/java/com/dio/desafio/desafio_dio_api_rest/DesafioDioApiRestApplication.java
+++ src/main/java/com/dio/desafio/desafio_dio_api_rest/DesafioDioApiRestApplication.java
@@ -1,8 +1,11 @@
 package com.dio.desafio.desafio_dio_api_rest;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default server url")})
 @SpringBootApplication
 public class DesafioDioApiRestApplication {
 
